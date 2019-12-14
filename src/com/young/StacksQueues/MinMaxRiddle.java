a53c043a9b66c2f2dc040889918f57d475220589
“Young <hogusong1@gmail.com>
2019-12-14 01:18:21 -0500

    Max of Min Riddle

--- src/com/young/StacksQueues/MinMaxRiddle.java
+++ src/com/young/StacksQueues/MinMaxRiddle.java
@@ -1,4 +1,64 @@
 package com.young.StacksQueues;
 
+import java.util.*;
+
 public class MinMaxRiddle {
+
+    public static void main(String[] args) {
+
+    }
+
+    static int[] riddle(int[] arr) {
+        int[] lefts = buildLefts(arr);
+        int[] rights = buildRights(arr);
+
+        SortedMap<Integer, Integer> numberToLength = new TreeMap<>(Collections.reverseOrder());
+        for (int i = 0; i < arr.length; i++) {
+            numberToLength.put(arr[i], Math.max(numberToLength.getOrDefault(arr[i], -1), lefts[i] + rights[i] + 1));
+        }
+
+        Iterator<Integer> iter = numberToLength.keySet().iterator();
+        int number = iter.next();
+        int[] result = new int[arr.length];
+        for (int i = 0; i < result.length; i++) {
+            while (numberToLength.get(number) <= i) {
+                number = iter.next();
+            }
+
+            result[i] = number;
+        }
+        return result;
+    }
+
+    static int[] buildLefts(int[] arr) {
+        int[] lefts = new int[arr.length];
+
+        Stack<Integer> indices = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!indices.empty() && arr[i] <= arr[indices.peek()]) {
+                indices.pop();
+            }
+
+            lefts[i] = i - (indices.empty() ? -1 : indices.peek()) - 1;
+            indices.push(i);
+        }
+
+        return lefts;
+    }
+
+    static int[] buildRights(int[] arr) {
+        int[] rights = new int[arr.length];
+
+        Stack<Integer> indices = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!indices.empty() && arr[i] <= arr[indices.peek()]) {
+                indices.pop();
+            }
+
+            rights[i] = (indices.empty() ? arr.length : indices.peek()) - i - 1;
+            indices.push(i);
+        }
+
+        return rights;
+    }
 }
